3dffcd1a4785c5029043348f71efa3eedbeea1b0
stould <vinirresko@gmail.com>
2019-02-09 23:15:18 -0800

    Restructure project using Babel and Browserify

---
+++ libs/resourcesHandler.js
@@ -0,0 +1,9 @@
+export var loadResourceAsHtml = function(elementType = null, properties = null) {
+    var newElement = document.createElement(elementType);
+    if(properties) {
+        for(var i = 0; i < properties.length; i++) {
+            newElement.setAttribute(properties[i].name, properties[i].value);
+        }
+    }
+    return newElement;
+};
---
+++ libs/setup.js
@@ -0,0 +1,50 @@
+import  {loadResourceAsHtml} from '../libs/resourcesHandler';
+import {addDragAndDropToElement} from '../libs/dragAndDropHandler';
+
+const heartProperties = [
+    {
+        name: 'width',
+        value: '50',
+    },
+    {
+        name: 'height',
+        value: '50',
+    },
+];
+
+function loadHearts(count, container) {
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+
+    const heartSize = 50;
+
+    for(var i = 0; i < count; i++) {
+        const fileName = ('images/heart' + (i + 1) + '.svg');
+        var properties = heartProperties;
+        properties.push({
+            name: 'src',
+            value: fileName,
+        });
+        var newImg = loadResourceAsHtml('img', properties);
+        var newDiv = document.createElement('div');
+        newDiv.appendChild(newImg);
+
+        newDiv.style.position = 'absolute';
+        newDiv.style.top = Math.floor(Math.random() * (height - heartSize)) + 'px';
+        newDiv.style.left = Math.floor(Math.random() * (width - heartSize)) + 'px';
+
+        newDiv.ondragstart = function() {
+            return false;
+        };
+
+        addDragAndDropToElement(newDiv);
+        
+        container.appendChild(newDiv);
+    }
+}
+
+(function(){
+    var container = document.querySelector('#container');
+    container.style.position = 'relative';
+    loadHearts(6, container)
+})();
